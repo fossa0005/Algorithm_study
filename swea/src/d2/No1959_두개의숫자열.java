27bb89ede740604d2d4b09e588446117bd3ce8e3
fossa0005 <fossa0005@gmail.com>
2026-08-27 10:53:04 +0900

    1 solved

--- swea/src/d2/No1959_두개의숫자열.java
+++ swea/src/d2/No1959_두개의숫자열.java
@@ -7,46 +7,55 @@ public class No1959_두개의숫자열 {
 
 	public static void main(String[] args) { 
 		Scanner sc = new Scanner(System.in);
+		int[] longer;
+		int[] shorter;
 		//test
 		//N M
 		//N길이
 		//M길이
 		
-		//M이 더 길게 세팅
+		
 		
 		int test = sc.nextInt();
 		for(int t=1; t<=test; t++) {
 			int N = sc.nextInt();
 			int M = sc.nextInt();
-			if (N > M) {
-				int bigger = N;
-				N = M;
-				M = bigger;
+			
+			if(N<M) {
+				longer = new int[M];
+				shorter = new int[N];
+				
+				for(int i=0; i<N; i++) {
+					shorter[i] = sc.nextInt();
+				}
+				
+				for(int i=0; i<M; i++) {
+					longer[i] = sc.nextInt();
+				}
+				
+			} else {
+				longer = new int[N];
+				shorter = new int[M];
+				
+				for(int i=0; i<N; i++) {
+					longer[i] = sc.nextInt();
+				}
+				
+				for(int i=0; i<M; i++) {
+					shorter[i] = sc.nextInt();
+				}
+				
 			}
-			
-			int[] arr1 = new int[N];
-			int[] arr2 = new int[M];
-			
-			
-			
-			for(int i=0; i<N; i++) {
-				arr1[i] = sc.nextInt();
-			}
-			
-			for(int i=0; i<M; i++) {
-				arr2[i] = sc.nextInt();
-			}
-			
-			//System.out.println(Arrays.toString(arr1));
-			//System.out.println(Arrays.toString(arr2));
+		
 			
 			int res  = Integer.MIN_VALUE;
 			
-			for(int i=0; i<=M-N; i++) { //긴배열
+			for(int i=0; i<=longer.length-shorter.length; i++) { //긴배열 기준점
 				int sum = 0;
 				
-				for(int j=0; j<N; j++) {
-					sum += arr1[j] * arr2[i+j];
+				for(int j=0; j<shorter.length; j++) {
+					sum += shorter[j] * longer[i+j];
+					
 				}
 				
 				if(sum > res) {
