3116e1861317dcb07d866b63fd8a61b1a0252462
fossa0005 <fossa0005@gmail.com>
2026-09-22 23:57:15 +0900

    신뢰 해결
    신뢰 해결

--- swea/src/d3/No10761_신뢰.java
+++ swea/src/d3/No10761_신뢰.java
@@ -1,32 +1,92 @@
 package d3;
 
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class No10761_신뢰 {
-	static int time;
-	static int pos;
-	static ArrayList<Integer> buttonsB, buttonsO;
+	static int[] buttonsB, buttonsO; //[2, 4, 0, 0, 0, ....] [1, 2, 0, 0, 0, ...] 
+	static ArrayDeque<String> pushOrder; //버튼 누르는 순서 [B, O, O, B]
+	
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt();
-		
-		for(int n=0; n<N; n++) {
-			String robot = sc.next();
-			if(robot.equals("B")) {
-				buttonsB.add(sc.nextInt());
+		int T = sc.nextInt();
+		for (int t=1; t<=T; t++) {
+			int N = sc.nextInt();
+			buttonsB = new int[100];
+			buttonsO = new int[100];
+			pushOrder = new ArrayDeque<String>();
+			
+			for(int n=0, i=0, j=0; n<N; n++) {
+				String robot = sc.next();
+				if(robot.equals("B")) {
+					buttonsB[i++] = sc.nextInt();
+					pushOrder.addLast("B");
+				}
+				else {
+					buttonsO[j++] = sc.nextInt();
+					pushOrder.add("O");
+				}
+				
 			}
-			else buttonsO.add(sc.nextInt());
+			
+			
+			int time = 0;
+			int posB = 1; int posO = 1;
+			int idxB = 0; int idxO = 0; //목표 버튼 인덱스
+			
+			while(! pushOrder.isEmpty()) {
+				time++;
+				boolean flag = true; //버튼 누를 수 있는지(같은 초에 둘 중 하나만 누를수있음)
+				
+				//B
+				if(buttonsB[idxB] != 0) { //목표버튼 존재
+					//버튼으로 이동
+					if(posB != buttonsB[idxB]) { 
+						posB = buttonsB[idxB] < posB ? posB-1 : posB+1;
+					}
+					
+					//버튼 누르기
+					else if(flag && posB == buttonsB[idxB] && pushOrder.peek().equals("B")) {
+						idxB++;
+						flag = false;
+						pushOrder.pop();
+					}
+					//대기
+					else;
+				}
+				
+				//O
+				if(buttonsO[idxO] != 0) {
+					//버튼으로 이동
+					if(posO != buttonsO[idxO]) {
+						posO = buttonsO[idxO] < posO ? posO-1 : posO+1;
+					}
+					
+					//버튼 누르기
+					else if(flag && posO == buttonsO[idxO] && pushOrder.peek().equals("O")) {
+						idxO++;
+						pushOrder.pop();
+					}
+					//대기
+					else;
+				}
+			}
+			
+			System.out.println("#" + t + " " + time);
+			
 		}
 		
 		
-		
+	
 	}
+	
+	
+	
 
-	static void simulate(int[] buttons) {
-		for(int button : buttons) {
-			time += button - pos + 1;
-		}
-	}
+	
 }
+
+	
+
